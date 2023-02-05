9bc305b6f87ea1d7285e9283d303af6cf1a31fd6
David Q <drq@Azkaban.local>
2023-02-05 13:08:49 -0500

    Install newer version of containerd
    This must be done as the version in the Ubuntu 22.04.1 repo does
    not work with these scripts.
    
    This will install containerd from github/containerd version 1.6.12
    It will manually install the systemd unit file.

--- setup-container.sh
+++ setup-container.sh
@@ -68,7 +68,54 @@ then
 	sudo sysctl --system
 
 	# (Install containerd)
-	sudo apt-get update && sudo apt-get install -y containerd
+	sudo apt-get update && sudo apt-get install -y runc libc6
+	wget https://github.com/containerd/containerd/releases/download/v1.6.12/containerd-1.6.12-linux-amd64.tar.gz
+	tar xvf containerd-1.6.12-linux-amd64.tar.gz
+	cd bin
+	cp * /usr/bin/
+	cat <<-SYSTEMD | sudo tee /lib/systemd/system/containerd.service
+# Copyright The containerd Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+[Unit]
+Description=containerd container runtime
+Documentation=https://containerd.io
+After=network.target local-fs.target
+
+[Service]
+ExecStartPre=-/sbin/modprobe overlay
+ExecStart=/usr/bin/containerd
+
+Type=notify
+Delegate=yes
+KillMode=process
+Restart=always
+RestartSec=5
+# Having non-zero Limit*s causes performance problems due to accounting overhead
+# in the kernel. We recommend using cgroups to do container-local accounting.
+LimitNPROC=infinity
+LimitCORE=infinity
+LimitNOFILE=infinity
+# Comment TasksMax if your systemd version does not supports it.
+# Only systemd 226 and above support this version.
+TasksMax=infinity
+OOMScoreAdjust=-999
+
+[Install]
+WantedBy=multi-user.target
+	SYSTEMD
+
 	# Configure containerd
 	sudo mkdir -p /etc/containerd
 	cat <<- TOML | sudo tee /etc/containerd/config.toml
@@ -84,7 +131,8 @@ version = 2
             SystemdCgroup = true
 	TOML
 
-	# Restart containerd
-	sudo systemctl restart containerd	
+	systemctl daemon-reload
+	systemctl start containerd
+
 fi
 
